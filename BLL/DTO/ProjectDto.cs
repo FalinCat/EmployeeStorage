21bc1e433093dd0777550d6caa1b6c7c1ce33ff7
FalinCat <42151288+FalinCat@users.noreply.github.com>
2022-02-11 20:38:10 +0200

    Make ID nullable

--- BLL/DTO/ProjectDto.cs
+++ BLL/DTO/ProjectDto.cs
@@ -4,6 +4,7 @@ namespace BLL.DTO
 {
     public class ProjectDto
     {
+        public Guid? Id { get; set; }
         public string Name { get; set; }
 
         public ProjectStatus Status { get; set; }
