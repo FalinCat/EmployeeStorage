ba2d01cc502ef0533d0a8a1213aaf9646d26d8d2
FalinCat <42151288+FalinCat@users.noreply.github.com>
2022-02-11 20:52:23 +0200

    Add roles to Authorize attribute

--- EmployeerStorageAPI/Controllers/SkillsController.cs
+++ EmployeerStorageAPI/Controllers/SkillsController.cs
@@ -2,13 +2,13 @@
 using AutoMapper;
 using BLL.DTO;
 using BLL.Services.Interfaces;
+using DAL.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeerStorageAPI.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
     public class SkillsController : ControllerBase
     {
         private readonly ISkillService _skillService;
@@ -20,16 +20,18 @@ namespace EmployeerStorageAPI.Controllers
             _mapper = mapper;
         }
 
+
         // GET: api/Skills
-        [HttpGet, Authorize(Roles = "Administrator")]
+        [HttpGet, Authorize(Roles = "Administrator,Moderator,User")]
         public ActionResult<IEnumerable<SkillDto>> GetSkills()
         {
             var skills = _skillService.GetMany().ToList();
             return _mapper.Map<List<SkillDto>>(skills);
         }
 
+
         // GET: api/Skills/5
-        [HttpGet("{id}")]
+        [HttpGet("{id}"), Authorize(Roles = "Administrator,Moderator,User")]
         public ActionResult<SkillDto> GetSkill(Guid id)
         {
             var skill = _skillService.Get(x => x.Id == id);
@@ -40,6 +42,43 @@ namespace EmployeerStorageAPI.Controllers
             }
 
             return _mapper.Map<SkillDto>(skill);
+        }
+
+
+        // POST: api/Skills
+        [HttpPost, Authorize(Roles = "Administrator,Moderator")]
+        public async Task<ActionResult<SkillDto>> PostSkill(SkillDto skill)
+        {
+            Skill newSkill;
+            if (skill.Id == null)
+            {
+                newSkill = new Skill() { Name = skill.Name };
+            }
+            else
+            {
+                newSkill = _mapper.Map<Skill>(skill);
+            }
+            
+            _skillService.Add(newSkill);
+            _skillService.SaveChanges();
+
+            return CreatedAtAction("GetSkill", new { id = newSkill.Id }, newSkill);
+        }
+
+
+        // DELETE: api/Projects/5
+        [HttpDelete("{id}"), Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> DeleteSkill(Guid id)
+        {
+            var skill = _skillService.Get(x => x.Id == id);
+            if (skill == null)
+            {
+                return NotFound();
+            }
+            _skillService.Remove(skill);
+            _skillService.SaveChanges();
+
+            return NoContent();
         }
     }
 }
