6d80189ec55bbf9c481c3a563d952d90a0f38ac9
FalinCat <42151288+FalinCat@users.noreply.github.com>
2022-02-11 20:39:39 +0200

    Add reverse mapping

--- BLL/MappingProfile.cs
+++ BLL/MappingProfile.cs
@@ -8,13 +8,19 @@ namespace BLL
     {
         public MappingProfile()
         {
-            // Add as many of these lines as you need to map your objects
             CreateMap<City, CityDto>();
             CreateMap<Company, CompanyDto>();
             CreateMap<Employee, EmployeeDto>();
             CreateMap<Project, ProjectDto>();
             CreateMap<Role, RoleDto>();
             CreateMap<Skill, SkillDto>();
+
+            CreateMap<CityDto, City>();
+            CreateMap<CompanyDto, Company>();
+            CreateMap<EmployeeDto, Employee>();
+            CreateMap<ProjectDto, Project>();
+            CreateMap<RoleDto, Role>();
+            CreateMap<SkillDto, Skill>();
         }
     }
 }
